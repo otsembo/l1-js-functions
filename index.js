0f22f605bb6cbe3170d76ac297b058ec31d99e88
Ian Okumu <okumu.otsembo@gmail.com>
2022-11-22 12:11:10 +0300

    Complete

--- index.js
+++ index.js
@@ -5,18 +5,38 @@ function printValue(value){
 
 // 1.0 FUNCTION DECLARATION
 //TODO: create a basic function that adds two random values
+function add(){
+    const num1 = 4
+    const num2 = 20
+    printValue(num1 + num2)
+}
+
+add()
 
 
 //TODO: create a function that takes in two values and adds them
-
-
+function addNums(n1, n2){
+    printValue(n1 + n2)
+}
+addNums(5, 108)
+addNums(13, 18)
 //TODO: create a function that takes in two values, adds them and returns the value of the addition
+function addNumbers(n1, n2){
+    const sum = n1 + n2
+    return sum
+}
+const summation = addNumbers(45, 37)
+printValue(summation)
+
+
 
 
 // 1.0 FUNCTION EXPRESSION
 //TODO: create a basic function that adds two random values
-
-
+const addExp = function(){
+    printValue(310 + 50)
+}
+addExp()
 //TODO: create a function that takes in two values and adds them
 
 
@@ -25,7 +45,13 @@ function printValue(value){
 
 // 1.0 ARROW FUNCTION
 //TODO: create a basic function that adds two random values
+const addAnonymous = () => {
+    printValue(60+90)
+}
+addAnonymous()
 
+const sayHello = () => printValue("Hello")
+sayHello()
 
 //TODO: create a function that takes in two values and adds them
 
